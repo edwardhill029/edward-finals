a02c5f7903c5257bd28a5479cae34e737b276b5e
edwardhill029 <hilledward29@gmail.com>
2024-05-26 19:42:14 +0800

    finalexamcommit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,41 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+    <section class="section dashboard">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
+            <div class="col-xxl-4 col-md-6">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Number of Posts:</h5>
+                        <div class="ps-3">
+                            <h6>{{ $totalPosts }}</h6>
+                        </div>
+                    </div>
+                </div>    
             </div>
+            <div class="col-xxl-4 col-md-6">
+            <div class="card info-card sales-card">
+                <div class="card-body">
+                    <h5 class="card-title">Unpublished of Posts:</h5>
+                    <div class="ps-3">
+                        <h6>{{ $unpublishedPosts }}</h6>
+                    </div>
+                </div>
+            </div>    
         </div>
-    </div>
+        <div class="col-xxl-4 col-md-6">
+            <div class="card info-card sales-card">
+                <div class="card-body">
+                    <h5 class="card-title">Published of Posts:</h5>
+                    <div class="ps-3">
+                        <h6>{{ $publishedPosts }}</h6>
+                    </div>
+                </div>
+            </div>    
+        </div>
+
+        
+    </section>
 </x-app-layout>
